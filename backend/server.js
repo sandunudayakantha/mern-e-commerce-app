d03f1bcb4feb853849483d398259e99ca62f83fe
sandunudayakantha <sndnudayakantha@gmail.com>
2025-02-13 22:35:06 +0530

    bk end

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
+
+const app  =  express();
+
+const PORT = process.env.PORT || 5001
+
+//authnication
+
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT, ()=>{
+    console.log("sever is running on http://localhost:"+ PORT);
+})
